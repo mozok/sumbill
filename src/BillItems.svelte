c4490c26cd671710fcf7ec2ab7127a96a7c47dfe
mozokevgen <mozokevgen@gmail.com>
2020-04-22 22:33:24 +0300

    add items component

---
+++ src/BillItems.svelte
@@ -0,0 +1,44 @@
+<script>
+    import { bills } from './stores.js';
+    export let index;
+    export let billItems;
+    let name = '';
+    $: total = billItems.reduce((sum, value) => 
+        parseFloat(value) ? sum + parseFloat(value) : sum, 0);
+
+    function changeItem(itemIndex, item) {
+        bills.updateBill(index, itemIndex, item);
+    }
+</script>
+
+<div class="container">
+    <input type="text" bind:value={name} placeholder="Name">
+
+    {#each billItems as item, itemIndex}
+        <div>
+            <input 
+                type="text"
+                bind:value={item}
+                on:change={changeItem(itemIndex, item)}
+                placeholder="Cost"
+            >
+        </div>
+    {/each}
+
+    {#if name}
+        <div>
+            <span>Total for {name}: {total}</span>
+        </div>
+    {/if}
+    <div>
+        <button on:click={bills.addItem(index)}>+</button>
+        <button on:click={bills.removeItem(index)}>-</button>
+    </div>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
